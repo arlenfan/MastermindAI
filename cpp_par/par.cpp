a7bd83b60feaf517fc50df8b2087faa3e09621be
arlenfan <arlenefan0928@gmail.com>
2023-03-23 16:14:32 -0700

    continuning testing

--- cpp_par/par.cpp
+++ cpp_par/par.cpp
@@ -2,31 +2,69 @@
 #include<vector>
 #include<algorithm>
 #include <execution>
-#include "current_time.cpp"
+
+
+
+#ifndef CURRENT_TIME_H
+#define CURRENT_TIME_H
+
+#include <chrono>
+#include <cstdint>
+
+class CurrentTime {
+    std::chrono::high_resolution_clock m_clock;
+
+public:
+//    uint64_t milliseconds();
+//    uint64_t microseconds();
+//    uint64_t nanoseconds();
+    uint64_t milliseconds()
+    {
+        return std::chrono::duration_cast<std::chrono::milliseconds>
+                (m_clock.now().time_since_epoch()).count();
+    }
+
+    uint64_t microseconds()
+    {
+        return std::chrono::duration_cast<std::chrono::microseconds>
+                (m_clock.now().time_since_epoch()).count();
+    }
+
+    uint64_t nanoseconds()
+    {
+        return std::chrono::duration_cast<std::chrono::nanoseconds>
+                (m_clock.now().time_since_epoch()).count();
+    }
+};
+
+#endif  /* CURRENT_TIME_H */
 
 using namespace std;
+
 int main() {
-	  std::vector<int>foo(1000000);
-		CurrentTime current_time;
-	      uint64_t start1 = current_time.milliseconds();
+    std::vector<int> foo(10000);
+    CurrentTime current_time;
 
-	  std::for_each(
-			      std::execution::par_unseq,
-			          foo.begin(),
-				      foo.end(),
-				          [](auto&& item)
-					      {
-					      		//cout<< item <<endl;
-							    });
+    uint64_t start1 = current_time.milliseconds();
 
-	      uint64_t end1 = current_time.milliseconds();
-	      cout<<"ms: "<<(end1-start1)<<endl;
-		start1=current_time.milliseconds();
-			      for (auto i=foo.begin();i!=foo.end();++i){
-				//
-			      }
-			      end1=current_time.milliseconds();
-			      cout<<"ms: "<<(end1-start1)<<endl;
+    std::for_each(
+            std::execution::par_unseq,
+            foo.begin(),
+            foo.end(),
+            [](auto &&item) {
+                //cout<< item <<endl;
+            });
 
-	      return 0;
+
+    uint64_t end1 = current_time.milliseconds();
+    cout<<(end1-start1)<<endl;
+    start1 = current_time.milliseconds();
+    for (auto i = foo.begin(); i != foo.end(); ++i) {
+        //
+    }
+    end1 = current_time.milliseconds();
+    cout<<(end1-start1)<<endl;
+    cout<<"Done"<<endl;
+
+    return 0;
 }
