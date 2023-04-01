3ae3e6e6b608c569b5263b841464f57b8eb2b69e
Arlen Fan <arlenefan0928@gmail.com>
2023-04-01 15:33:46 -0500

    perms complete

--- java-old/GFG.java
+++ java-old/GFG.java
@@ -1,20 +1,27 @@
-
+import java.util.Arrays;
+import java.util.*;
 class GFG {
+
+    public static List<int[]> all =new ArrayList<>();
 
     static void CombinationRepetitionUtil(int chosen[], int arr[],
             int index, int r, int start, int end) {
         if (index == r) {
+        int[] temp = new int[r];
             for (int i = 0; i < r; i++) {
-                System.out.printf("%d ", arr[chosen[i]]);
+//                 System.out.printf("%d ", arr[chosen[i]]);
+                temp[i] = arr[chosen[i]];
             }
-            System.out.printf("\n");
+            all.add(temp);
+//             System.out.printf("\n");
             return;
         }
         for (int i = start; i <= end; i++) {
             chosen[index] = i;
             CombinationRepetitionUtil(chosen, arr, index + 1,
                     r, i, end);
-        }
+
+                   }
         return;
     }
 
@@ -28,5 +35,14 @@ class GFG {
         int n = arr.length;
         int r = 5;
         CombinationRepetition(arr, n, r);
+
+        for (int i = 0 ; i<all.size();++i){
+            System.out.println();
+                   for (int j = 0 ; j<all.get(i).length;++j){
+                    System.out.print(all.get(i)[j]);
+                   }
+
+        }
     }
+
 }
